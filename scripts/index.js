db2e13ef54129b3278f80e6ed56528cd9d3d32f8
Igor Nikolaev <nikolaev.iv@bk.ru>
2023-10-10 13:55:50 +0300

    для 1-го ревью

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,27 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsList = document.querySelector('.places__list');
+
+function createCard (card, deleteFunction) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    cardElement.querySelector('.card__image').src = card.link;
+    cardElement.querySelector('.card__title').textContent = card.name;
+
+    deleteButton.addEventListener('click', deleteFunction);
+
+    return cardElement;
+}
+
+function deleteCard(evt) {
+    const card = evt.target.closest('.card');
+    card.remove();
+}
+
+initialCards.forEach((item) => {
+    cardsList.append(createCard(item, deleteCard));
+});
