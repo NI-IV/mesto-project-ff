0bd79eaffa38e904098991959c95f070673d7f7e
Igor Nikolaev <nikolaev.iv@bk.ru>
2023-11-03 15:12:43 +0300

    реализованы попапы, добавление карточек. Для проверки 6 спринт

---
+++ src/components/card.js
@@ -0,0 +1,31 @@
+
+const cardTemplate = document.querySelector("#card-template").content;
+
+
+// Функция создания карточки путем клонирования темплейта
+
+export function createCard(card, deleteFunction, openPopupCardFunc, likeFunc) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const cardLikeButton = cardElement.querySelector(".card__like-button");
+
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
+  cardTitle.textContent = card.name;
+
+  deleteButton.addEventListener("click", deleteFunction);
+  cardImage.addEventListener('click', openPopupCardFunc);
+  cardLikeButton.addEventListener('click', likeFunc);
+
+  return cardElement;
+}
+
+
+// Функция удаления карточки
+
+export function deleteCard(evt) {
+  const card = evt.target.closest(".card");
+  card.remove();
+}
---
+++ src/scripts/index.js
@@ -0,0 +1,148 @@
+
+import "../pages/index.css";
+import { initialCards } from "../components/cards";
+import { openPopup, closePopup } from "../components/modal";
+import { createCard, deleteCard } from "../components/card";
+
+/*-----------------------------------------------------------------------------------
+    КОНСТАНТЫ И ПЕРЕМЕННЫЕ
+*/
+
+// Список карточек
+
+const cardsList = document.querySelector(".places__list");
+
+// Кнопки
+
+const profileEditButton = document.querySelector(".profile__edit-button");
+const newCardButton = document.querySelector(".profile__add-button");
+
+// ПРОФИЛЬ на странице
+
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+
+// Попап ПРОФИЛЬ
+
+const popupProfile = document.querySelector(".popup_type_edit");
+const popupProfileForm = document.forms['edit-profile'];
+const popupProfileNameInput = popupProfileForm.elements.name;
+const popupProfileDescriptionInput = popupProfileForm.elements.description;
+
+// Попап НОВАЯ КАРТОЧКА
+
+const popupAddCard = document.querySelector(".popup_type_new-card");
+const popupAddCardForm = document.forms['new-place'];
+const popupAddCardNameInput = popupAddCardForm.elements['place-name'];
+const popupAddCardLinkInput = popupAddCardForm.elements.link;
+
+// Попап КАРТОЧКА
+
+const popupCard = document.querySelector(".popup_type_image");
+const popupCardImage = document.querySelector(".popup__image");
+const popupCardName = document.querySelector(".popup__caption");
+
+// Кнопки закрытия попапов
+
+const closeButtons = document.querySelectorAll('.popup__close');
+
+
+/*-----------------------------------------------------------------------------------
+    
+*/
+
+
+// Выводит карточки на страницу
+
+initialCards.forEach((item) => {
+  cardsList.append(createCard(item, deleteCard, openPopupImage, likeCard));
+});
+
+
+
+// Слушатель клика по кнопке редактирования профиля
+
+profileEditButton.addEventListener("click", () => {
+  popupProfileNameInput.value = profileName.textContent;
+  popupProfileDescriptionInput.value = profileDescription.textContent;
+
+  openPopup(popupProfile);
+});
+
+
+
+// Слушатель клика по кнопке добавления карточки
+
+newCardButton.addEventListener("click", () => {
+  openPopup(popupAddCard);
+});
+
+
+
+// Слушатель закрытия на все кнопки попапов
+
+closeButtons.forEach(item => {
+  item.addEventListener("click", (evt) => {
+    const popup = evt.target.closest('.popup');
+
+    closePopup(popup);
+  })
+})
+
+
+
+// Функция открытия попапа с изображением
+
+function openPopupImage(evt) {
+  popupCardImage.src = evt.target.src;
+  popupCardImage.alt = evt.target.alt;
+  popupCardName.textContent = evt.target.alt;
+
+  openPopup(popupCard);
+}
+
+
+
+// Коллбэк добавления лайка
+
+function likeCard(evt) {
+  evt.target.classList.toggle("card__like-button_is-active");
+}
+
+
+
+// Коллбэк сохранения данных формы изменения профиля
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileName.textContent = popupProfileNameInput.value;
+  profileDescription.textContent = popupProfileDescriptionInput.value;
+
+  closePopup(popupProfile);
+}
+
+// Слушатель клика по кнопке сохранения формы профиля
+
+popupProfileForm.addEventListener('submit', handleFormSubmit);
+
+
+
+// Коллбэк сохранения данных формы добавления карточки
+
+function handleCardFormSubmit(evt) {
+  evt.preventDefault();
+
+  const card = {};
+  card.name = popupAddCardNameInput.value;
+  card.link = popupAddCardLinkInput.value;
+
+  cardsList.prepend(createCard(card, deleteCard, openPopupImage, likeCard));
+
+  closePopup(popupAddCard);
+  popupAddCardForm.reset();
+}
+
+// Слушатель клика по кнопке сохранения формы добавления карточки
+
+popupAddCardForm.addEventListener('submit', handleCardFormSubmit);
